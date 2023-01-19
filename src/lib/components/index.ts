7b8202ff0a939594825841b3dc2e50d41636d699
garrett <gnelson@duck.com>
2023-01-19 15:52:56 -0500

    Add input component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -2,10 +2,6 @@ import PageTitle from './PageTitle.svelte'
 import Logo from './Logo.svelte'
 import MenuLink from './MenuLink.svelte'
 import MenuLinkGroup from './MenuLinkGroup.svelte'
+import Input from './Input.svelte'
 
-export {
-	PageTitle,
-	Logo,
-	MenuLink,
-	MenuLinkGroup
-}
+export { PageTitle, Logo, MenuLink, MenuLinkGroup, Input }
