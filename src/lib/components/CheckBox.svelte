e76c6f5550514fc1b8474e5ad29aedff430896bf
garrett <gnelson@duck.com>
2023-01-20 10:14:42 -0500

    Create checkbox component

---
+++ src/lib/components/CheckBox.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements'
+	interface $$Props extends HTMLInputAttributes {
+		id: string
+		label: string
+		error?: string
+		inputClass?: string
+		containerClass?: string
+		checked?: boolean
+	}
+
+	export let id: string
+	export let label: string
+	export let error: string | undefined = undefined
+	export let inputClass = ''
+	export let containerClass = ''
+	export let checked = false
+</script>
+
+<div class="form-control {containerClass}">
+	<label
+		for={id}
+		class="label"
+	>
+		<input
+			class="checkbox {inputClass} {error ? 'checkbox-error' : ''}"
+			{id}
+			name={id}
+			type="checkbox"
+			bind:checked
+			{...$$restProps}
+		/>
+		<span class="label-text">{label}</span>
+	</label>
+	{#if error}
+		<label
+			for={id}
+			class="label"
+		>
+			<span class="label-text-alt text-error">{error}</span>
+		</label>
+	{/if}
+</div>
