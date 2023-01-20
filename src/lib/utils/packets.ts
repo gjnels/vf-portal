0e6e4b14e267f8846ae087030cf3163c4cb03882
garrett <gnelson@duck.com>
2023-01-20 10:16:10 -0500

    Create styles for each nicotine packet color

---
+++ src/lib/utils/packets.ts
@@ -0,0 +1,42 @@
+export const packetColors: { [key: string]: { border: string; background: string } } = {
+	yellow: {
+		border: 'border-yellow-500',
+		background: 'bg-yellow-500'
+	},
+	green: {
+		border: 'border-green-500',
+		background: 'bg-green-500'
+	},
+	blue: {
+		border: 'border-sky-500',
+		background: 'bg-sky-500'
+	},
+	red: {
+		border: 'border-red-500',
+		background: 'bg-red-500'
+	},
+	purple: {
+		border: 'border-purple-600',
+		background: 'bg-purple-600'
+	},
+	brown: {
+		border: 'border-amber-800',
+		background: 'bg-amber-800'
+	},
+	black: {
+		border: 'border-black',
+		background: 'bg-black'
+	},
+	orange: {
+		border: 'border-orange-500',
+		background: 'bg-orange-500'
+	},
+	pink: {
+		border: 'border-pink-500',
+		background: 'bg-pink-500'
+	},
+	grey: {
+		border: 'border-gray-500',
+		background: 'bg-gray-500'
+	}
+}
