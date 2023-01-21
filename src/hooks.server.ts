0347c74b272ba6efa64dc91b9f06627a49b7a069
garrett <gnelson@duck.com>
2023-01-21 15:44:17 -0500

    Create server handle hook to protect account routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,2 +1,15 @@
 import '$lib/supabase'
 import '$lib/server/supabase'
+import type { Handle } from '@sveltejs/kit'
+import { requireUser } from '$lib/server/supabase'
+
+export const handle = (async ({ event, resolve }) => {
+	// Account routes requires a logged in user
+	if (event.url.pathname.startsWith('/account')) {
+		await requireUser(event)
+	}
+
+	const response = await resolve(event)
+
+	return response
+}) satisfies Handle
