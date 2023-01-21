e117a6536ea855a23bca92582abd421f145be6ac
garrett <gnelson@duck.com>
2023-01-21 18:58:55 -0500

    Add profile and password zod schemas

--- src/lib/schemas/users.ts
+++ src/lib/schemas/users.ts
@@ -1,3 +1,4 @@
+import { Role } from '@prisma/client'
 import { z } from 'zod'
 
 export const loginSchema = z.object({
@@ -10,3 +11,38 @@ export const loginSchema = z.object({
 		.min(1, { message: 'Password is required' })
 		.trim()
 })
+
+export const profileSchema = z.object({
+	name: z
+		.string()
+		.transform((x) => x || null)
+		.nullable(),
+	role: z.nativeEnum(Role).optional(),
+	location: z
+		.string()
+		.nullish()
+		.transform((l) => (l === '' ? null : l))
+})
+
+export const passwordSchema = z
+	.object({
+		password: z
+			.string({ required_error: 'Enter your new password' })
+			.min(8, { message: 'Password must be 8 or more characters' })
+			.max(64),
+		passwordConfirm: z.string({ required_error: 'Confirm your new password' })
+	})
+	.superRefine(({ password, passwordConfirm }, ctx) => {
+		if (password !== passwordConfirm) {
+			ctx.addIssue({
+				code: 'custom',
+				message: 'Passwords must match',
+				path: ['password']
+			})
+			ctx.addIssue({
+				code: 'custom',
+				message: 'Passwords must match',
+				path: ['passwordConfirm']
+			})
+		}
+	})
