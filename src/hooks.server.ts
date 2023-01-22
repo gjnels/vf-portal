bbbefbcf118f67ee3bf53148f7c6ec29115c5b43
garrett <gnelson@duck.com>
2023-01-21 19:15:54 -0500

    Protect all admin routes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,17 @@ import '$lib/supabase'
 import '$lib/server/supabase'
 import type { Handle } from '@sveltejs/kit'
 import { requireUser } from '$lib/server/supabase'
+import { Admin } from '$lib/roles'
 
 export const handle = (async ({ event, resolve }) => {
 	// Account routes requires a logged in user
 	if (event.url.pathname.startsWith('/account')) {
 		await requireUser(event)
+	}
+
+	// Admin routes require Admin role
+	if (event.url.pathname.startsWith('/admin')) {
+		await requireUser(event, { roles: Admin })
 	}
 
 	const response = await resolve(event)
