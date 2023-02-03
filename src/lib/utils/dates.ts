7e3b353530ea5ded45dbfb8934a24cc14d64368e
garrett <gnelson@duck.com>
2023-02-03 13:41:06 -0500

    Add new date formats

--- src/lib/utils/dates.ts
+++ src/lib/utils/dates.ts
@@ -3,3 +3,7 @@ import advancedFormat from 'dayjs/plugin/advancedFormat'
 dayjs.extend(advancedFormat)
 
 export const formatPromoDate = (date: Date) => dayjs(date).format('dddd, MMM Do, YYYY h:mma')
+
+export const formatPromoTableDate = (date: Date) => dayjs(date).format('h:mm a\ndddd\nMMM Do, YYYY')
+
+export const formatDateInput = (date: Date | string) => dayjs(date).format('YYYY-MM-DDTHH:mm')
